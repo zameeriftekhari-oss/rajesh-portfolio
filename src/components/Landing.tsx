25dcc8ae624a6a880e6fa735a049fade6f000a70
zameeriftekhari-oss <zameer.iftekhari@gmail.com>
2026-03-13 14:55:21 +0800

    Update Landing.tsx
    Zamir Landing page

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -9,20 +9,20 @@ const Landing = ({ children }: PropsWithChildren) => {
           <div className="landing-intro">
             <h2>Hello! I'm</h2>
             <h1>
-              RAJESH
+              ZAMIR IFTEKHARI
               <br />
               <span>CHITYAL</span>
             </h1>
           </div>
           <div className="landing-info">
-            <h3>A Full Stack</h3>
+            <h3>A Graphic Designer</h3>
             <h2 className="landing-info-h2">
-              <div className="landing-h2-1">Developer</div>
-              <div className="landing-h2-2">Engineer</div>
+              <div className="landing-h2-1">Product Developer</div>
+              <div className="landing-h2-2">Innovator & Creator</div>
             </h2>
             <h2>
-              <div className="landing-h2-info">Engineer</div>
-              <div className="landing-h2-info-1">Developer</div>
+              <div className="landing-h2-info">Product Developer</div>
+              <div className="landing-h2-info-1">Innovator & Creator</div>
             </h2>
           </div>
         </div>
